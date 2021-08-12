062309e97b52a9ed5cf25864ec3586ee9153b137
MayarHisham666 <mayar_myhome2@yahoo.com>
2021-08-12 09:39:11 +0200

    Take input from user

---
+++ Task3.py
@@ -0,0 +1,66 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter The First Number =4\n",
+      "Enter The Operation:+\n",
+      "Enter The Second Number =5\n",
+      "9.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "x1=float(input(\"Enter The First Number =\"))\n",
+    "c=input(\"Enter The Operation:\")\n",
+    "x2=float(input(\"Enter The Second Number =\"))\n",
+    "if c=='+':\n",
+    "    print(x1+x2)\n",
+    "elif c=='-':\n",
+    "    print(x1-x2)\n",
+    "elif c=='*':\n",
+    "    print(x1*x2)\n",
+    "elif c=='/':\n",
+    "    print(x1/x2)\n",
+    "else:\n",
+    "    print(\"Invalid operation\")\n",
+    "    \n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
