5315e42c393f361b4d7bae7ea45bbcd9efa805c6
MayarHisham666 <mayar_myhome2@yahoo.com>
2021-08-12 13:24:48 +0200

    Call module Assignment_3

---
+++ Call_module.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import Assignment_3
+
+
+# In[3]:
+
+
+Assignment_3.Calculator(3,4,"+")
+
+
+# In[5]:
+
+
+Assignment_3.slope(2,2,1,4)
+
+
+# In[7]:
+
+
+Assignment_3.waterTempreture(60)
+
+
+# In[ ]:
+
+
+
+
