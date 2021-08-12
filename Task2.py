61b3e752bad01a62d1082b4f80c887b551f52a35
MayarHisham666 <mayar_myhome2@yahoo.com>
2021-08-12 09:30:15 +0200

    Convert ascii representation

---
+++ Task2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list=["s","q","a"]
+for i in range(len(list)):
+    print("The ascii represenation of: ",list[i]," is ",ord(list[i]))
+
+
+# In[2]:
+
+
+list=[45,78,102]
+for i in range(len(list)):
+    print("The ascii represenation of: ",list[i]," is ",chr(list[i]))
+
+
+# In[ ]:
+
+
+
+
