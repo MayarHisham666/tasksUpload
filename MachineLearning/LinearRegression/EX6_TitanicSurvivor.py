cde4ec4b202848f56ef457262b5cf48066e53b66
MayarHisham666 <mayar_myhome2@yahoo.com>
2021-11-13 12:51:05 +0200

    'DecisionTree'

--- MachineLearning/LinearRegression/EX6_TitanicSurvivor.py
+++ MachineLearning/LinearRegression/EX6_TitanicSurvivor.py
@@ -7,77 +7,144 @@
 import pandas as pd
 
 
-# In[6]:
+# In[2]:
 
 
 titanic=pd.read_csv('titanic.csv')
 titanic.head(30)
 
 
-# In[49]:
+# In[3]:
 
 
 titanicList=titanic[['Survived','Pclass','Name','SibSp','Parch','Fare','Embarked']]
 titanicList
 
 
-# In[50]:
+# In[4]:
 
 
 titanicList_dummies=pd.get_dummies(titanicList.Embarked)
 titanicList_dummies
 
 
-# In[51]:
+# In[5]:
 
 
 titanicList_concat=pd.concat([titanicList,titanicList_dummies],axis='columns')
 titanicList_concat
 
 
-# In[52]:
+# In[6]:
 
 
 titanicData=titanicList_concat.drop("Embarked",axis='columns')
 titanicData
 
 
-# In[53]:
+# In[7]:
 
 
 x=titanicData.drop("Name",axis='columns')
 y=titanicData.Name
 
 
-# In[54]:
+# In[8]:
 
 
 from sklearn import tree
 model=tree.DecisionTreeClassifier()
 
 
-# In[55]:
+# In[9]:
 
 
 model.fit(x,y)
 
 
-# In[56]:
+# In[10]:
 
 
 model.score(x,y)
 
 
-# In[58]:
+# In[11]:
 
 
 model.predict([[1,3,1,0,47.3,0,0,1]])
 
 
-# In[59]:
+# In[12]:
 
 
 model.predict([[1,1,1,0,86.74,0,1,0]])
+
+
+# In[79]:
+
+
+tt=titanic[['Pclass','Sex','Age','Fare',]]
+tt.head(10)
+
+
+# In[80]:
+
+
+tt.fillna(0.0,inplace=True)
+tt.head(10)
+
+
+# In[81]:
+
+
+from sklearn.preprocessing import LabelEncoder
+tt['sex_n']=LabelEncoder().fit_transform(tt.Sex)
+td=tt.drop('Sex',axis='columns')
+td
+
+
+# In[89]:
+
+
+inputs=td.drop('sex_n',axis='columns')
+inputs
+
+
+# In[90]:
+
+
+target=td.sex_n
+target
+
+
+# In[91]:
+
+
+modelII=tree.DecisionTreeClassifier()
+
+
+# In[92]:
+
+
+modelII.fit(inputs,target)
+
+
+# In[93]:
+
+
+modelII.score(inputs,target)
+
+
+# In[94]:
+
+
+modelII.predict([[3,22,54.000]])
+
+
+# In[95]:
+
+
+modelII.predict([[2,7,8.700]])
 
 
 # In[ ]:
